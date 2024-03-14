fefab5314bb4920cf25eaab20685d0b5854b875a
kevely10 <163428636+kevely10@users.noreply.github.com>
2024-03-14 12:00:40 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,12 @@
+numero1=int(input("insira o primero numero"))
+numero2=int(input("insira o segundo numero"))
+
+adicao=numero1+numero2
+subtracao=numero1-numero2
+multiplicacao=numero1*numero2
+divisao=numero1/numero2
+
+print("a soma dos numeros e " , adicao)
+print("a subtracao dos numeros e " , subtracao)
+print("a multiplicacao os numeros e " , multiplicacao)
+print("a divisao dos numeros e " , divisao)
